66d35f49af3d3a8a5745ffcd1ff52f82d07d3f46
alexnsu <alexnssm@gmail.com>
2018-01-26 16:21:32 +0100

    Normalized the data

--- test.py
+++ test.py
@@ -12,17 +12,21 @@ def read_data():
 def split_data(data):
     defect_col = len(data['attributes']) - 1
 
-    defects = [x for x in data['data'] if x[defect_col] == 'Y']
-    correct = [x for x in data['data'] if x[defect_col] == 'N']
+    correct = np.array([x[0:defect_col] for x in data['data'] if x[defect_col] == 'N'], dtype='f')
+    defects = np.array([x[0:defect_col] for x in data['data'] if x[defect_col] == 'Y'], dtype='f')
 
     if not len(defects) + len(correct) == len(data['data']):
         raise AssertionError('Data did not split properely!')
+
+    data_argmax = np.amax(np.array([x[0:defect_col] for x in data['data']]), axis=0)
+
+    correct = correct / data_argmax
+    defects = defects / data_argmax
 
     return correct, defects
 
 def main():
     data = read_data()
-
     correct, defects = split_data(data)
 
 if __name__ == '__main__':
